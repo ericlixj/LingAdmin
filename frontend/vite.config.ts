a1bfbd4ecf292660b44dacbaf08723adc00ac489
lig <lig@t3go.cn>
2025-06-18 10:33:12 -0700

    init

---
+++ frontend/vite.config.ts
@@ -0,0 +1,20 @@
+import path from "node:path"
+import { TanStackRouterVite } from "@tanstack/router-vite-plugin"
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  plugins: [react(), TanStackRouterVite()],
+  server: {
+    watch: {
+      usePolling: true,
+      interval: 100, // 轮询间隔（毫秒）
+    }
+  },
+})
